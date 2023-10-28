6c4631d296617021147cfb2439150e1205b38e2f
Noman Rafique <108799714+NaumanRafique12@users.noreply.github.com>
2023-10-29 03:56:41 +0500

    Update feature_extraction.py

--- Bollywood Celebrity/feature_extraction.py
+++ Bollywood Celebrity/feature_extraction.py
@@ -11,6 +11,7 @@ Original file is located at
 # !pip install tensorflow==2.3.1
 # !pip install keras==2.4.3
 # !pip install keras-vggface==0.6
+
 # !pip install keras_applications==1.0.8
 
 #import os
